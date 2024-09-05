df1ff8b4deec63fccd3c00281e91405f3f1d75ae
Angelina Egorova <linaaa.egorova@gmail.com>
2024-09-05 21:36:08 +0300

    The first task was solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
-"use strict"
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = b ** 2 - 4 * a * c;
+	let arr = [];
+
+	if (d == 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+	};
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+	percent = percent / (100 * 12);
+	let s = amount - contribution;
+	let payMonth = s * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+	let totalMortgage = payMonth * countMonths;
+
+	return (Math.round(totalMortgage * 100) / 100);
+};
